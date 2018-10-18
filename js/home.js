789b4044037af4fd83e5763dd6594fd89af3988f
Mitsuyama <1462973856@qq.com>
2018-10-18 09:47:31 +0800

    temple

--- js/home.js
+++ js/home.js
@@ -3,37 +3,54 @@ var boxUp = document.getElementsByClassName("boxUp");
 var boxDown = document.getElementsByClassName("boxDown");
 var itermWidth = iterms[0].offsetWidth;
 var infoText = document.getElementsByClassName("infoText");
+var presentHeight = 0;
+var titleList = new Array();
+var timeList = new Array();
+var briefList = new Array();
+var orderList = new Array();
 
-/*
-function setOp(obj, oP) {
-    obj.getElementsByClassName("briefIntroContext")[0].style.opacity = parseFloat(100 - oP) / 100.0 + "";
-    obj.getElementsByClassName("sawNumBox")[0].style.opacity = parseFloat(100 - oP) / 100.0 + "";
-    obj.getElementsByClassName("boxDown")[0].style.opacity = parseFloat(100 - oP) / 100.0 + "";
-    obj.getElementsByClassName("boxInter")[0].style.opacity = parseFloat(100 - oP) / 100.0 + "";
-    //obj.getElementsByClassName("briefIntroContextMore")[0].style.opacity = parseFloat(oP) / 100.0 + "";
+function waterfall() {
+    var pageWidth = document.getElementsByClassName("text_container")[0].offsetWidth;
+    var gap = 19;
+    var columns = parseInt(pageWidth / (itermWidth + gap));
+    var arr = [];
+    var maxHeight = 0;
+    for(var i = 0; i < iterms.length; i++) {
+        if(i < columns) {
+            iterms[i].style.top = gap + "px";
+            iterms[i].style.left = i * (itermWidth + gap) + "px";
+            arr.push(iterms[i].offsetHeight + gap);
+            if(maxHeight < iterms[i].offsetHeight + gap) {
+                maxHeight = iterms[i].offsetHeight + gap
+            }
+        }
+        else {
+            var index = 0;
+            var minHeight = arr[0];
+            for(var j = 0; j < arr.length; j++) {
+                if(minHeight > arr[j]) {
+                    index = j;
+                    minHeight = arr[j];
+                }
+            }
+            iterms[i].style.top = arr[index] + gap + "px";
+            iterms[i].style.left = iterms[index].offsetLeft + "px";
+            arr[index] = arr[index] + iterms[i].offsetHeight + gap;
+            for(var j = 0; j < arr.length; j++) {
+                if(maxHeight < arr[j]) {
+                    maxHeight = arr[j];
+                }
+            }
+        }
+    }
+    for(var i = 0; i < infoText.length; i++) {
+        if(infoText[i].offsetHeight < 60) {
+            infoText[i].style.top = 13 + "px";
+        }
+    }
+    document.getElementsByClassName("home_background")[0].style.height = maxHeight + 100 + "px";
+    presentHeight = Math.max(presentHeight, maxHeight);
 }
-
-function anotherSet(obj, oP) {
-    obj.getElementsByClassName("briefIntroContextMore")[0].style.opacity = parseFloat(oP) / 100.0 + "";
-}
-
-function mouseOnButton(obj) {
-    for(var i = 0; i <= 100; i += 25) {
-        setTimeout(setOp, i * 2, obj, i);
-    }
-    for(var i = 0; i <= 100; i += 10) {
-        setTimeout(anotherSet, i * 2, obj, i);
-    }
-}
-
-function mouseOutButton(obj) {
-    for(var i = 0; i <= 100; i += 25) {
-        setTimeout(setOp, i * 2, obj, 10 - i);
-    }
-    for(var i = 0; i <= 100; i += 10) {
-        setTimeout(anotherSet, i * 2, obj, 10 - i);
-    }
-}*/
 
 function pageLoading() {
     var screenWidthRate = parseFloat($(document).width()) / 1920.0;
@@ -56,58 +73,30 @@ function pageLoading() {
     }
 
     waterfall();
+}
 
-    function waterfall() {
-        var pageWidth = document.getElementsByClassName("text_container")[0].offsetWidth;
-        var gap = 19;
-        var columns = parseInt(pageWidth / (itermWidth + gap));
-        var arr = [];
-        var maxHeight = 0;
-        for(var i = 0; i < iterms.length; i++) {
-            if(i < columns) {
-                iterms[i].style.top = gap + "px";
-                iterms[i].style.left = i * (itermWidth + gap) + "px";
-                arr.push(iterms[i].offsetHeight + gap);
-                if(maxHeight < iterms[i].offsetHeight + gap) {
-                    maxHeight = iterms[i].offsetHeight + gap
-                }
-            }
-            else {
-                var index = 0;
-                var minHeight = arr[0];
-                for(var j = 0; j < arr.length; j++) {
-                    if(minHeight > arr[j]) {
-                        index = j;
-                        minHeight = arr[j];
-                    }
-                }
-                iterms[i].style.top = arr[index] + gap + "px";
-                iterms[i].style.left = iterms[index].offsetLeft + "px";
-                arr[index] = arr[index] + iterms[i].offsetHeight + gap;
-                for(var j = 0; j < arr.length; j++) {
-                    if(maxHeight < arr[j]) {
-                        maxHeight = arr[j];
-                    }
-                }
-            }
-        }
-        for(var i = 0; i < infoText.length; i++) {
-            if(infoText[i].offsetHeight < 60) {
-                infoText[i].style.top = 13 + "px";
-            }
-        }
-        document.getElementsByClassName("home_background")[0].style.height = maxHeight + 100 + "px";
+window.onresize = function() {
+    waterfall();
+};
+
+$(window).scroll(function(event){
+    if($(window).scrollTop() + $(window).height() > presentHeight) {
+
     }
+});
 
-    window.onresize = function() {
-        waterfall();
-    };
+function loadXml() {
+    var xmlDoc = document.implementation.createDocument("", "", null);
+    xmlDoc.async = "false";
+    xmlDoc.load("../xml/waterfall.xml");
 }
 
 $(document).ready(function(){
     $("#homeBackground").hide();
     $(".nevigation").hide();
     $(".home_background").hide();
+    loadXml();
+
     $(window).load(function(){
         $("#homeBackground").fadeIn();
         $(".nevigation").fadeIn();
--- makeHTML.py
+++ makeHTML.py
@@ -11,6 +11,7 @@ class makeHTML:
     homeBttonTemp = []
     blogTemp = []
     num = 0
+    xmlFile = None
 
     def __init__(self):
         path = 'md/'  #待读取的文件夹
@@ -252,10 +253,13 @@ class makeHTML:
         #start = input()
         #end = input()
         self.indexFile = open('index.html', 'w')
+        self.xmlFile = open('xml/waterfall.xml', 'w')
+        
         homePage = self.homeTemp
         content = ''
         
-        for i in range(self.num - 1, -1, -1):
+        maxNum = 10
+        for i in range(self.num - 1, max(self.num - maxNum, 0) -1, -1):
             self.pHtml(i)
             buttonTemp = self.homeBttonTemp
             buttonTemp = buttonTemp.replace('((blogHref))', str(i))
@@ -265,6 +269,29 @@ class makeHTML:
             buttonTemp = buttonTemp.replace('((brief))', self.blogInfo[0][:-1])
             buttonTemp = buttonTemp.replace('((Tag))', self.blogInfo[1][:-1])
             content += buttonTemp
+        
+        self.xmlFile.write('<content>\n')
+        if self.num > maxNum:
+            for i in range(self.num - maxNum - 1, -1, -1):
+                self.pHtml(i)
+                self.xmlFile.write('<box>\n')
+                self.xmlFile.write('<order>')
+                self.xmlFile.write(str(i))
+                self.xmlFile.write('</order>\n')
+                self.xmlFile.write('<title>')
+                self.xmlFile.write(self.blogInfo[2][:-1])
+                self.xmlFile.write('</title>\n')
+                self.xmlFile.write('<time>')
+                self.xmlFile.write(self.blogInfo[3][:-1])
+                self.xmlFile.write('</time>\n')
+                self.xmlFile.write('<brief>')
+                self.xmlFile.write(self.blogInfo[0][:-1])
+                self.xmlFile.write('</brief>\n')
+                self.xmlFile.write('<tag>')
+                self.xmlFile.write(self.blogInfo[1][:-1 ])
+                self.xmlFile.write('</tag>\n')
+                self.xmlFile.write('</box>\n')
+        self.xmlFile.write('</content>')
         
         homePage = homePage.replace('((mainContainer))', content)
         self.indexFile.write(homePage)
