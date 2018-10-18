47ef4865017076404a922455814ccfa177eb4e2c
Mitsuyama <1462973856@qq.com>
2018-10-18 11:42:34 +0800

    ajax test

--- js/home.js
+++ js/home.js
@@ -4,6 +4,7 @@ var boxDown = document.getElementsByClassName("boxDown");
 var itermWidth = iterms[0].offsetWidth;
 var infoText = document.getElementsByClassName("infoText");
 var presentHeight = 0;
+var xmlhttp;
 var titleList = new Array();
 var timeList = new Array();
 var briefList = new Array();
@@ -87,27 +88,27 @@ $(window).scroll(function(event){
 
 function loadXML(){
     var xmlDoc;
-    try { //IE
-        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-        }
-    catch(e) { //firefox,opera
-        xmlDoc = document.implementation.createDocument("", "", null);
+
+    xmlhttp = null;
+    if (window.XMLHttpRequest) { // code for all new browsers
+        xmlhttp = new XMLHttpRequest();
+    } 
+    else if (window.ActiveXObject) { // code for IE5 and IE6
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    try {
-        xmlDoc.asyc = false; //是否异步调用
-        xmlDoc.load("../xml/waterfall.xml"); //文件路径
-    }
-    catch(e) { //chrome
-        var xmlhttp = new window.XMLHttpRequest();
-        xmlhttp.open("GET", "../xml/waterfall.xml", false); //创建一个新的http请求，并指定此请求的方法、URL以及验证信息
+    if (xmlhttp != null) {
+        xmlhttp.open("GET", "xml/waterfall.xml", true);
+        alert(xmlhttp.status);
+        //xmlhttp.send(null);
         xmlDoc = xmlhttp.responseXML;
     }
+
     return xmlDoc;
 }
 
 function loadXml() {
     var xmlDoc = loadXML();
-    alert(xmlDoc);
+    //alert(xmlDoc);
 }
 
 $(document).ready(function(){
