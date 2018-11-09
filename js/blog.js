014d5f514e7e91f157e5824fe6c59bbe1831f7c3
Mitsuyama <1462973856@qq.com>
2018-11-09 12:45:54 +0800

    update

--- js/blog.js
+++ js/blog.js
@@ -85,13 +85,14 @@ $(document).ready(function() {
                 imgHeight = this.height;
                 $('.imgBack').css("height", $(document.body).height() + 2000 + 'px');
                 $('.imgBack').html('<img src = ' + imgSrc + ' class = "biggerImg"></img>');
-                if(imgHeight * 0.8 < $(window).height()) {
+                var shWitdh = 0;
+                //alert(imgWidth + ' & ' + $(window).width() * 0.8);
+                if(imgHeight < $(window).height()) {
                     $('.biggerImg').css("top", parseFloat($(window).scrollTop()) / screenWidthRate + parseFloat(($(window).height() - imgHeight)) / 2.0 + 'px');
                 }
                 else {
                     $('.biggerImg').css("top", parseFloat($(window).scrollTop()) / screenWidthRate + 'px');
                 }
-                alert(imgHeight * 0.8 + ' & ' + $(window).height());
                 shWitdh = Math.min($(window).width() * 0.8, imgWidth) + 'px';
                 $('.biggerImg').animate({width: shWitdh}, tempTime);
             });
